7a7e33e3dbcd27f69d6274c69660c7f194503c91
Victor Wong <vj1901@hotmail.com>
2017-01-17 21:34:18 +0000

    added request-as-json.js

---
+++ request-as-json.js
@@ -0,0 +1,18 @@
+var request = require('request');
+
+function requestJson (url, callback) {
+    request(url, function(err, response) {
+        if (err) {
+            callback(err)
+        }
+        else {
+            try {
+                var x = JSON.parse(response.body);
+                callback(null, x)
+            }
+            catch(err) {
+                callback(err)
+            }
+        }
+    })
+}
