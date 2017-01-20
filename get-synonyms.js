810c470e02b2f4b1c7e367343afeaaf37b9fc5e1
Victor Wong <vj1901@hotmail.com>
2017-01-20 15:18:41 +0000

    get synonyms.js completed

--- get-synonyms.js
+++ get-synonyms.js
@@ -15,67 +15,173 @@
 // Prompt the user for a word
 // Using your API, retrieve the synonyms/antonyms/etc. for the input word
 // If everything goes well, display all the results to the user in a nice way
-
+var colors = require('colors');
 var prompt = require('prompt');
-// var request = require('request');
 
 var getSynonyms = require("./library/synonyms.js");
 
-// var bigURL = "http://words.bighugelabs.com/api/2/34f7de7253e531a60ff98736f56ced36/"; //WORD/json
-
-// http://words.bighugelabs.com/api/2/34f7de7253e531a60ff98736f56ced36/magic/json
-// var apiKey = "34f7de7253e531a60ff98736f56ced36";
-
 prompt.get(['word'], function(err, word) {
-   
     if (err) {
         console.log("There is an error in the first section.");
     }
     else {
-        // word = word.
-        // word = word.toString();
         word = word.word;
-        // console.log(typeof word); 
-        // console.log(word);
         var newWord = new getSynonyms("34f7de7253e531a60ff98736f56ced36");
         newWord.getSynonyms(word, function(err, result) {
-            // console.log('hey2');
             if (err) {
-                console.log("There is an error in the second section.");
+                console.log("There is an error in the second section." + err);
             }
             else {
+                // setting up noun possibilities
+                var nounSYN = result.noun ? result.noun.syn : null;
+                var nounANT = result.noun ? result.noun.ant : null;
+                var nounSIM = result.noun ? result.noun.sim : null;
+                var nounREL = result.noun ? result.noun.rel : null;
+                
+                // setting up adjective possibilities
+                var adjectiveSYN = result.adjective ? result.adjective.syn : null;
+                var adjectiveANT = result.adjective ? result.adjective.ant : null;
+                var adjectiveSIM = result.adjective ? result.adjective.sim : null;
+                var adjectiveREL = result.adjective ? result.adjective.rel : null;
+                
+                // setting up verb possibilities
+                var verbSYN = result.verb ? result.verb.syn : null;
+                var verbANT = result.verb ? result.verb.ant : null;
+                var verbSIM = result.verb ? result.verb.sim : null;
+                var verbREL = result.verb ? result.verb.rel : null;
+                
+                // setting up adverb possibilities
+                var adverbSYN = result.abverb ? result.abverb.syn : null;
+                var adverbANT = result.adverb ? result.adverb.ant : null;
+                var adverbSIM = result.adverb ? result.adverb.sim : null;
+                var adverbREL = result.adverb ? result.adverb.rel : null;
+                
+                console.log("\nYou have selected " + word + ", and here is what we found: \n" );
+                
+                //printing out noun
+                console.log("NOUN                                             ".cyan.bold.underline);
+                console.log("Synonyms".green.underline.bold);
+                if(!nounSYN) {
+                    nounSYN = "";
+                    console.log(nounSYN);
+                } else {
+                    console.log(nounSYN + ".\n");
+                }
+                console.log("Antonyms".blue.underline.bold);
+                if(!nounANT) {
+                    nounANT = "";
+                    console.log(nounANT);
+                } else {
+                    console.log(nounANT + ".\n");
+                }
+                console.log("Similar".yellow.underline.bold);
+                if(!nounSIM) {
+                    nounSIM = "";
+                    console.log(nounSIM);
+                } else {
+                    console.log(nounSIM + ".\n");
+                }
+                console.log("Relation".magenta.underline.bold);
+                if(!nounREL) {
+                    nounREL = "";
+                    console.log(nounREL);
+                } else {
+                    console.log(nounREL + ".\n");
+                }
                 
                 
-                console.log("You have selected " + word + ", and here is what we found: ");
-                console.log(result);
+                //printing out adjective
+                console.log("ADJECTIVE                                             ".cyan.bold.underline);
+                if(!adjectiveSYN) {
+                    adjectiveSYN = "";
+                    console.log(adjectiveSYN);
+                } else {
+                    console.log(adjectiveSYN + ".\n");
+                }
+                console.log("Antonyms".blue.underline.bold);
+                if(!adjectiveANT) {
+                    adjectiveANT = "";
+                    console.log(adjectiveANT);
+                } else {
+                    console.log(adjectiveANT + ".\n");
+                }
+                console.log("Similar".yellow.underline.bold);
+                if(!adjectiveSIM) {
+                    adjectiveSIM = "";
+                    console.log(adjectiveSIM);
+                } else {
+                    console.log(adjectiveSIM + ".\n");
+                }
+                console.log("Relation".magenta.underline.bold);
+                if(!adjectiveREL) {
+                    adjectiveREL = "";
+                    console.log(adjectiveREL);
+                } else {
+                    console.log(adjectiveREL + ".\n");
+                }
+                
+                
+                //printing out verb
+                console.log("VERB                                             ".cyan.bold.underline);
+                if(!verbSYN) {
+                    verbSYN = "";
+                    console.log(verbSYN);
+                } else {
+                    console.log(verbSYN + ".\n");
+                }
+                console.log("Antonyms".blue.underline.bold);
+                if(!verbANT) {
+                    verbANT = "";
+                    console.log(verbANT);
+                } else {
+                    console.log(verbANT + ".\n");
+                }
+                console.log("Similar".yellow.underline.bold);
+                if(!verbSIM) {
+                    verbSIM = "";
+                    console.log(verbSIM);
+                } else {
+                    console.log(verbSIM + ".\n");
+                }
+                console.log("Relation".magenta.underline.bold);
+                if(!verbREL) {
+                    verbREL = "";
+                    console.log(verbREL);
+                } else {
+                    console.log(verbREL + ".\n");
+                }
+                
+                
+                // printing out adverb
+                console.log("ADVERB                                             ".cyan.bold.underline);
+                if(!adverbSYN) {
+                    adverbSYN = "";
+                    console.log(adverbSYN);
+                } else {
+                    console.log(adverbSYN + ".\n");
+                }
+                console.log("Antonyms".blue.underline.bold);
+                if(!adverbANT) {
+                    adverbANT = "";
+                    console.log(adverbANT);
+                } else {
+                    console.log(adverbANT + ".\n");
+                }
+                console.log("Similar".yellow.underline.bold);
+                if(!adverbSIM) {
+                    adverbSIM = "";
+                    console.log(adverbSIM);
+                } else {
+                    console.log(adverbSIM + ".\n");
+                }
+                console.log("Relation".magenta.underline.bold);
+                if(!adverbREL) {
+                    adverbREL = "";
+                    console.log(adverbREL);
+                } else {
+                    console.log(adverbREL + ".\n");
+                }
             }
         });
     }
-    //     synonym(apiKey);
-    //     var 
-        
-    // }
-        // var theWord = word;
-        // var myBigURL = bigURL + theWord + "/json";
-        // synonym(myBigURL, function(err, response) {
-        //     if (err) {
-        //         console.log("There is an error in the 2nd section.");
-        //     }
-        //     else {
-        //         var synWord = JSON.parse(response.body);
-        //         console.log(synWord);
-        //     }
-        // });
 });
-
-
-// adjective    
-// adverb       
-// noun         
-// verb         
-
-// synonym      syn
-// antonym      ant
-// relation     rel
-// similar      syn
-
